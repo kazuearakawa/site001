b549836d8584c5bce72796c725de3d2369994769
tomokun766 <tomokun766@gmail.com>
2017-12-25 00:29:55 +0900

    javascript作成中

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function(){
+  setTimeout(stopload, 10000);
   let imgCnt = 0;// 読み込んだ数カウント
   let Img = [];// イメージオブジェクト
   let IMG = [
@@ -7,14 +8,14 @@ window.addEventListener('DOMContentLoaded', function(){
     'images/sec2-ttl03.png','images/sec2-ttl04.png','images/sec2-ttl05.png',
     'images/sec2-ttl06.png','images/vibes.png','images/sec2-Mttl.png'
   ];
-    console.log(imgCnt + " / " + IMG.length);// 何枚中何枚目かを表示
+    // console.log(imgCnt + " / " + IMG.length);// 何枚中何枚目かを表示
 
   for(let i = 0; i < IMG.length; i++){
     Img[i] = new Image();
     Img[i].src = IMG[i];
     Img[i].onload = function() {//1枚の画像が読み込み完了したら呼び出し
     imgCnt++;// 読み込んだカウントアップ
-    console.log(imgCnt + " / " + IMG.length);// 何枚中何枚目かを表示
+    // console.log(imgCnt + " / " + IMG.length);// 何枚中何枚目かを表示
 
   if(imgCnt === IMG.length) loaded();// カウントと画像数が一緒になったら実行
   };
@@ -55,17 +56,35 @@ function loaded() {
   });
 };
 });
-
 /*load error*/
 //--------------------------------------------------------------------------
-window.addEventListener('DOMContentLoaded',function(){
-    setTimeout(stopload, 10000);
-  });
-   function stopload() {
-     let top = document.querySelector('.top');
-     let topInner = document.querySelector('.top-inner');
-     let topBg = document.querySelector('.top-bg');
-     top.classList.add('is-noOpaciy');
-     topInner.classList.add('is-noOpaciy');
-     topBg.classList.add('is-noOpaciy');
-     }
+function stopload() {
+  let top = document.querySelector('.top');
+  let topInner = document.querySelector('.top-inner');
+  let topBg = document.querySelector('.top-bg');
+  top.classList.add('is-noOpaciy');
+  topInner.classList.add('is-noOpaciy');
+  topBg.classList.add('is-noOpaciy');
+}
+
+// let body = document.body;
+// let html = document.documentElement;
+// let scrollTop = body.scrollTop || html.scrollTop;
+
+//
+// var scr_count = $(window).scrollTop(); // ページトップからのスクロール量を代入
+//
+// window.addEventListener('scroll' =>(){// スクロール毎にイベントが発火します。
+//   let target = document.querySelector('#target')// 発火させたい位置の取得
+//   let obj_t_pos = target.offset().top; // 変数
+//
+//   var scr_count = jQuery(document).scrollTop() + (window.innerHeight/2); // ディスプレイの半分の高さを追加
+//   if(scr_count > obj_t_pos){
+//      // jQuery('#target').addClass('red');
+//   }else{
+//      // jQuery('#target').removeClass();
+//   }
+//
+//    })
+//
+// })
